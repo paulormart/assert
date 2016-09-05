2e1804068fcbfe673e556b381b6839f54aa39e81
paulo <paulo.martins@aukbit.com>
2016-09-05 17:30:38 +0100

    removed TestingT struct

--- assert.go
+++ assert.go
@@ -2,15 +2,11 @@ package assert
 
 import (
 	"reflect"
+	"testing"
 )
 
-// TestingT is an interface wrapper around *testing.T
-type TestingT interface {
-	Errorf(format string, args ...interface{})
-}
-
 // Equal asserts two objects are deeply equal
-func Equal(t TestingT, expected, actual interface{}) bool {
+func Equal(t *testing.T, expected, actual interface{}) bool {
 
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("Not equal: %v (expected) != %v", expected, actual)
