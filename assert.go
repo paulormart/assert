a10381a4139c5f53ad77408b38111c0d1a8a6f9b
paulo <paulo.martins@aukbit.com>
2016-09-05 17:24:56 +0100

    initial commit

--- assert.go
+++ assert.go
@@ -2,7 +2,6 @@ package assert
 
 import (
 	"reflect"
-	"testing"
 )
 
 // TestingT is an interface wrapper around *testing.T
@@ -11,14 +10,12 @@ type TestingT interface {
 }
 
 // Equal asserts two objects are deeply equal
-func Equal(t *testing.T, expected, actual interface{}) bool {
+func Equal(t TestingT, expected, actual interface{}) bool {
 
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("Not equal: %v (expected) != %v", expected, actual)
 		return false
 	}
-
-
 	return true
 }
 
