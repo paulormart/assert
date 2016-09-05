5ef281d489bdafb51d0e45ffe3d1ce9e3fae8862
paulo <paulo.martins@aukbit.com>
2016-09-05 19:12:38 +0100

    add identifies the test caller file and print it

--- assert.go
+++ assert.go
@@ -1,16 +1,48 @@
+// Assert
+// ideas borrowed from https://github.com/stretchr/testify
 package assert
 
 import (
 	"reflect"
+	"fmt"
+	"runtime"
+	"strings"
 	"testing"
 )
 
-// Equal asserts two objects are deeply equal
+
+// getTestFile identifies the test file that has been called highlighting the line
+// Note: we assume a go test file always ends as _test.go
+func getTestFile() string {
+	for i := 0; ; i++ {
+		_, file, line, ok := runtime.Caller(i)
+
+		if !ok || file == "" || line == 0 {
+			break
+		}
+
+		if x := strings.LastIndex(file, "/"); x != -1 {
+			if name := file[x:]; strings.Contains(name, "_test.go"){
+				return fmt.Sprintf(".%s:%d", name, line)
+			}
+		}
+	}
+	return ""
+}
+
+// Fail get Caller file and print assertion
+func Fail(t *testing.T, expected, actual interface{}) bool {
+	if caller := getTestFile(); caller != "" {
+		t.Errorf("\rError at %v\t Not equal: %v (expected) != %v", caller, expected, actual)
+	}
+	return false
+}
+
+// Equal asserts deep equal for two objects
 func Equal(t *testing.T, expected, actual interface{}) bool {
 
 	if !reflect.DeepEqual(expected, actual) {
-		t.Errorf("Not equal: %v (expected) != %v", expected, actual)
-		return false
+		return Fail(t, expected, actual)
 	}
 	return true
 }
