3c27329e8682f983f1b7c6cc474ea37c412c2ab0
paulo <paulo.martins@aukbit.com>
2016-10-26 14:33:25 +0100

    added struct to tests

--- assert_test.go
+++ assert_test.go
@@ -1,37 +1,61 @@
 package assert_test
 
 import (
-	"testing"
 	"assert"
+	"testing"
 )
 
-type User struct {
-	name 	string
+type FakeStruct struct {
+	SomeSlice       []string
+	SomeStruct      FakeInnerStruct
+	SomeSliceStruct []FakeInnerStruct
+}
+type FakeInnerStruct struct {
+	ID      string
+	SomeMap map[int]string
 }
 
-func TestAll(t *testing.T){
+func TestAll(t *testing.T) {
 	// Table tests
-	var tests = []struct{
+	var tests = []struct {
 		expected interface{}
-		actual interface{}
-		result bool
-	}{{
-		expected: "Hello World",
-		actual: "Hello World",
-		result: true,
-	},
-	{
-		expected: nil,
-		actual: nil,
-		result: true,
-	},
-	{
-		expected: func()*User{
-			return &User{}
-		}(),
-		actual: &User{},
-		result: true,
-	}}
+		actual   interface{}
+		result   bool
+	}{
+		{
+			expected: "Hello World",
+			actual:   "Hello World",
+			result:   true,
+		},
+		{
+			expected: nil,
+			actual:   nil,
+			result:   true,
+		},
+		{
+			expected: func() *FakeStruct {
+				return &FakeStruct{}
+			}(),
+			actual: &FakeStruct{},
+			result: true,
+		},
+		{
+			expected: map[string]string{"key1": "foo", "key2": "bar"},
+			actual:   map[string]string{"key1": "foo", "key2": "bar"},
+			result:   true,
+		},
+		{
+			expected: FakeStruct{
+				SomeSlice:       []string{"foo", "bar"},
+				SomeStruct:      FakeInnerStruct{ID: "abc", SomeMap: map[int]string{1: "foo", 2: "bar"}},
+				SomeSliceStruct: []FakeInnerStruct{{ID: "abc", SomeMap: map[int]string{1: "foo", 2: "bar"}}}},
+			actual: FakeStruct{
+				SomeSlice:       []string{"foo", "bar"},
+				SomeStruct:      FakeInnerStruct{"abc", map[int]string{1: "foo", 2: "bar"}},
+				SomeSliceStruct: []FakeInnerStruct{{"abc", map[int]string{1: "foo", 2: "bar"}}}},
+			result: true,
+		},
+	}
 	//TODO add more tests
 
 	for _, test := range tests {
